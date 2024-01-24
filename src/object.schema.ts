555bf9b9f4b82d8047bc3a97168f3d321a1c8c5b
AKK-soft-dev <aungkokosoftdev@gmail.com>
2024-01-24 12:43:52 +0630

    test date type

--- src/object.schema.ts
+++ src/object.schema.ts
@@ -3,10 +3,15 @@ import { z } from "zod";
 const User = z.object({
   username: z.string(),
   email: z.string().email().optional(),
+  createdAt: z.date().readonly(),
 });
 
-User.parse({ username: "Ludwig", email: "aungkoko@gmail.com" });
+User.parse({
+  username: "Ludwig",
+  email: "aungkoko@gmail.com",
+  createdAt: new Date(),
+});
 
 // extract the inferred type
 type User = z.infer<typeof User>;
-// { username: string, email?: string | undefined }
+// { username: string, email?: string | undefined, createdAt: Date }
