883eb53ae9ca7a394adb8ca6a66465481156df60
AKK-soft-dev <aungkokosoftdev@gmail.com>
2024-01-24 13:00:56 +0630

    test email coercion

--- src/type-coercions.ts
+++ src/type-coercions.ts
@@ -7,3 +7,6 @@ console.log(typeof stringSchema.parse(true)); // => "true"
 const dateSchema = z.coerce.date();
 console.log(dateSchema.parse("Wed, 24 Jan 2024 06:24:18 GMT")); // Date object
 console.log(dateSchema.parse(Date.now()).getMonth()); // => current month represent in number type
+
+const emailSchema = z.coerce.string().email().min(4);
+console.log(emailSchema.parse("aungkoko@gmail.com")); // will throw ZodError if you pass invalid email string or string which length is less than 4
