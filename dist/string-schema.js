7f21e757e47206524d54137504dbb7535c32417e
AKK-soft-dev <aungkokosoftdev@gmail.com>
2024-01-24 11:44:05 +0630

    log error message

--- dist/string-schema.js
+++ dist/string-schema.js
@@ -4,8 +4,23 @@ const zod_1 = require("zod");
 // creating a schema for strings
 const mySchema = zod_1.z.string();
 // parsing
-mySchema.parse("tuna"); // => "tuna"
-mySchema.parse(12); // => throws ZodError
+try {
+    console.log(mySchema.parse("tuna")); // => "tuna"
+    mySchema.parse(12); // => throws ZodError
+}
+catch (err) {
+    if (err instanceof zod_1.ZodError) {
+        console.log(err.errors);
+    }
+}
 // "safe" parsing (doesn't throw error if validation fails)
-mySchema.safeParse("tuna"); // => { success: true; data: "tuna" }
-mySchema.safeParse(12); // => { success: false; error: ZodError }
+try {
+    console.log(mySchema.safeParse("tuna")); // => { success: true; data: "tuna" }
+    const validationErrorRes = mySchema.safeParse(12); // => { success: false; error: ZodError }
+    console.log(validationErrorRes);
+}
+catch (err) {
+    if (err instanceof zod_1.ZodError) {
+        console.log(err.errors);
+    }
+}
--- src/string-schema.ts
+++ src/string-schema.ts
@@ -1,12 +1,25 @@
-import { z } from "zod";
+import { ZodError, z } from "zod";
 
 // creating a schema for strings
 const mySchema = z.string();
 
 // parsing
-mySchema.parse("tuna"); // => "tuna"
-mySchema.parse(12); // => throws ZodError
+try {
+  console.log(mySchema.parse("tuna")); // => "tuna"
+  mySchema.parse(12); // => throws ZodError
+} catch (err) {
+  if (err instanceof ZodError) {
+    console.log(err.errors);
+  }
+}
 
 // "safe" parsing (doesn't throw error if validation fails)
-mySchema.safeParse("tuna"); // => { success: true; data: "tuna" }
-mySchema.safeParse(12); // => { success: false; error: ZodError }
+try {
+  console.log(mySchema.safeParse("tuna")); // => { success: true; data: "tuna" }
+  const validationErrorRes = mySchema.safeParse(12); // => { success: false; error: ZodError }
+  console.log(validationErrorRes);
+} catch (err) {
+  if (err instanceof ZodError) {
+    console.log(err.errors);
+  }
+}
